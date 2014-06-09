dc62acee49f7563537ce6052174d164afd6ffd0d
Can <can@codingday.com>
2014-06-09 08:04:49 +0100

    sql like

--- PropertySearch/routes/index.js
+++ PropertySearch/routes/index.js
@@ -57,7 +57,11 @@ function connecttodb(res, query) {
 
     var db = getMysqlConnection();
     db.connect();
-    db.query('SELECT *, (  (SELECT  price   FROM properties p2 WHERE p2.id = properties.id and p2.retrieved_date < properties.retrieved_date  order by  retrieved_date desc limit 1)) as previousprice FROM properties limit 10', function (appsError, apps)
+
+    var sql = 'SELECT *, (  (SELECT  price   FROM properties p2 WHERE p2.id = properties.id and p2.retrieved_date < properties.retrieved_date  order by  retrieved_date desc limit 1)) as previousprice FROM properties';
+    sql += 'description like =\'%'+query+'%\' limit 10';
+
+    db.query(sql, function (appsError, apps)
     {
         if (appsError) {
             console.log(appsError);
