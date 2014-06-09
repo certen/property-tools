9e170634f87378ab0edcd866fd0d7dc17793d2c0
Can <can@codingday.com>
2014-06-09 08:49:35 +0100

    escape

--- PropertySearch/routes/index.js
+++ PropertySearch/routes/index.js
@@ -28,7 +28,7 @@ function processProperties(rows, res, query)
 
         }
     }
-    console.log(query);
+
     res.render('search',
         {
             query: query,
@@ -54,9 +54,9 @@ function connecttodb(res, query) {
 
     var db = getMysqlConnection();
     db.connect();
-    var criteria = '\'%'+query+'%\'' ;
+    var criteria = '%'+query+'%' ;
 
-    //criteria = db.escape(criteria);
+    criteria = db.escape(criteria);
 
     var sql = 'SELECT *, (  (SELECT  price   FROM properties p2 WHERE p2.id = properties.id and p2.retrieved_date < properties.retrieved_date  order by  retrieved_date desc limit 1)) as previousprice FROM properties';
     sql += ' where title like '+criteria+'  or  description like '+criteria+' limit 20';
